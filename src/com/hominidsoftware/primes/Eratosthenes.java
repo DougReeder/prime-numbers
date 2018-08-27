3f0fad5a967f7df35698d4b70f6bc8959a6873cb
P. Douglas Reeder <reeder.29@gmail.com>
2018-08-27 12:03:59 -0400

    Adds basic implementation

--- src/com/hominidsoftware/primes/Eratosthenes.java
+++ src/com/hominidsoftware/primes/Eratosthenes.java
@@ -1,5 +1,7 @@
 package com.hominidsoftware.primes;
 
+import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.List;
 
 public class Eratosthenes implements PrimeNumberGenerator {
@@ -8,7 +10,22 @@ public class Eratosthenes implements PrimeNumberGenerator {
         if (startingValue < 0 || endingValue < 0) {
             throw new IllegalArgumentException("endpoints must be 0 or greater");
         }
-        return null;
+
+        BitSet sieve = new BitSet(endingValue+1);
+        sieve.set(2, endingValue+1);
+
+        int currentPrime = 1;   // not actually a prime
+        while ((currentPrime = sieve.nextSetBit(currentPrime+1)) >= 0) {
+            for (int number=currentPrime*2; number<=endingValue; number+=currentPrime) {
+                sieve.clear(number);
+            }
+        }
+
+        List<Integer> primes = new ArrayList<>();
+        for (int i = sieve.nextSetBit(0); i >= 0; i = sieve.nextSetBit(i+1)) {
+            primes.add(i);
+        }
+        return primes;
     }
 
     @Override
--- test/com/hominidsoftware/primes/EratosthenesTest.java
+++ test/com/hominidsoftware/primes/EratosthenesTest.java
@@ -2,6 +2,10 @@ package com.hominidsoftware.primes;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EratosthenesTest {
@@ -20,5 +24,13 @@ class EratosthenesTest {
             eratosthenes.generate(4, -1);
         });
     }
-    
+
+
+    @Test
+    void should_return_small_primes() {
+        List<Integer> primes = eratosthenes.generate(0, 19);
+
+        List<Integer> smallPrimes = new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19));
+        assertEquals(smallPrimes, primes);
+    }
 }
